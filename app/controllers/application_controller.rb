f47f999bff74e83c81f294fa90c1cfba243d32b1
Carl Tanner <carl@wdwhub.net>
2015-01-02 14:34:14 -0800

    ENHANCEMENT: nav bar automatically adds neighborhoods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  @neighborhoods = Neighborhood.all
+  before_action :init_neighborhood
+  
+  private
+  
+  def init_neighborhood
+    @neighborhoods = Neighborhood.all
+  end
+  
 end
