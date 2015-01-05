c1f25ffd3e4fc7dd739c58d0b28b4872f32b18e3
Carl Tanner <carl@wdwhub.net>
2015-01-04 16:29:46 -0800

    NF: Slideshow of a collection featured properties

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @locations = Location.all.reverse_order.in_groups_of(3, false)
+    @locations_first_three = @locations.shift
   end
 end
