3abec9c1b8a56903e4d9e0178b25bfff9053cb45
Carl Tanner <carl@wdwhub.net>
2015-01-10 08:50:34 -0800

    changed receiver of email for quote request

--- app/mailers/quote_request_mailer.rb
+++ app/mailers/quote_request_mailer.rb
@@ -1,8 +1,9 @@
 class QuoteRequestMailer < ApplicationMailer
   
   def quote_request_email(quote_request)
+    quote_receiver = Profile.last.email_address || "rocketeer.captproton@gmail.com"
     @quote_request = quote_request
-    mail( :to => "carl@wdwhub.net",
+    mail( :to => quote_receiver,
     :subject => 'Super Quote Request' )
 
   end
